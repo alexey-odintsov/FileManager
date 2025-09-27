d831e9687fc297bc0dc0e218b7405f2b8ee03cfe
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-09-27 17:13:07 +0200

    Window state

--- composeApp/src/jvmMain/kotlin/com/alekso/filemanager/main.kt
+++ composeApp/src/jvmMain/kotlin/com/alekso/filemanager/main.kt
@@ -1,12 +1,17 @@
 package com.alekso.filemanager
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
+    val state = rememberWindowState(width = 1280.dp, height = 720.dp)
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "FileManager",
+        state = state
     ) {
         App()
     }
