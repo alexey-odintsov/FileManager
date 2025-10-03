ad3e59506eb5bddefa3dd5874e4cc5c8ebcafb69
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-10-03 23:55:03 +0200

    File preview selection and scrolling added

--- composeApp/src/jvmMain/kotlin/com/alekso/filemanager/ui/FilesPanels.kt
+++ composeApp/src/jvmMain/kotlin/com/alekso/filemanager/ui/FilesPanels.kt
@@ -1,13 +1,20 @@
 package com.alekso.filemanager.ui
 
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.alekso.filemanager.model.FileEntry
@@ -32,7 +39,20 @@ actual fun FilesPanels(
             FilesList(currentDirectory, selectedFile, callbacks)
         }
         second(20.dp) {
-            Text(selectedFileContent.toString())
+            val scrollState = rememberScrollState()
+            Box {
+                Column(Modifier.verticalScroll(scrollState)) {
+                    SelectionContainer {
+                        Text(selectedFileContent.toString())
+                    }
+                }
+                VerticalScrollbar(
+                    modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+                    adapter = rememberScrollbarAdapter(
+                        scrollState = scrollState
+                    )
+                )
+            }
         }
         splitter {
             visiblePart {
